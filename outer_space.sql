13f9257272781615997e67712be01fdebbc6ee88
Jenny Liu <jennyliuye@gmail.com>
2021-09-14 16:50:25 -0700

    first part

---
+++ outer_space.sql
@@ -0,0 +1,54 @@
+  
+ CREATE TABLE stars(
+      name VARCHAR(100) PRIMARY KEY,
+      star_temp_k INT NOT NULL
+  );
+
+INSERT INTO stars(name, star_temp_k)
+VALUES('The Sun', 5800), ('Proxima Centauri', 3042), ('Gliese 876', 3192);
+
+ CREATE TABLE planets(
+      name VARCHAR(100) PRIMARY KEY,
+      orbit_star TEXT NOT NULL REFERENCES stars,
+      orbital_period_yrs NUMERIC(10,3) NOT NULL 
+  ); 
+INSERT INTO planets(name, orbit_star, orbital_period_yrs)
+VALUES('Earth','The Sun', 1.00), ('Mars','The Sun',1.882), 
+('Venus', 'The Sun', 0.62), ('Proxima Centauri b','Proxima Centauri', 0.3),
+('Gliese 876 b','Gliese 876', 0.236);
+
+  CREATE TABLE moons(
+      name VARCHAR(100) PRIMARY KEY,
+      orbit_planet TEXT NOT NULL REFERENCES planets
+  );
+
+INSERT INTO moons(name, orbit_planet)
+VALUES('The Moon', 'Earth'), ('Phobos', 'Mars'), ('Deimos', 'Mars');
+
+
+SELECT planets.name, stars.name, COUNT(moons)
+FROM planets
+LEFT JOIN stars
+ON planets.orbit_star = stars.name
+LEFT JOIN moons
+ON moons.orbit_planet = planets.name
+GROUP BY planets.name, stars.name
+ORDER BY planets.name;
+
+SELECT planets.name, stars.name, COUNT(moons)
+FROM moons
+RIGHT JOIN planets
+ON moons.orbit_planet = planets.name
+RIGHT JOIN stars
+ON planets.orbit_star = stars.name
+GROUP BY planets.name, stars.name
+ORDER BY planets.name;
+
+SELECT planets.name, stars.name, COUNT(moons)
+FROM moons
+RIGHT JOIN planets
+ON moons.orbit_planet = planets.name
+JOIN stars
+ON planets.orbit_star = stars.name
+GROUP BY planets.name, stars.name
+ORDER BY planets.name;
